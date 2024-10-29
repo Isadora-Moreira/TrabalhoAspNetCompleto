4eb5292959d5bd101f1812f4062296bce294b8cc
Giovanna Vilela De Oliveira <giovannavileladeoliveira@gmail.com>
2024-10-28 21:56:22 -0300

    arrumar verificação de detele de autor

--- Controllers/AutoresController.cs
+++ Controllers/AutoresController.cs
@@ -8,18 +8,21 @@ namespace TrabalhoAspNet.Controllers
     {
         private readonly Contexto _context;
 
+        // contexto de banco de dados, usado para interagir com a tabela.
         public AutoresController(Contexto context)
         {
             _context = context;
         }
 
         // GET: Autores
+        //Retorna uma lista de todos os autores e exibe na view correspondente
         public async Task<IActionResult> Index()
         {
             return View(await _context.Autores.ToListAsync());
         }
 
         // GET: Autores/Details/5
+        // Exibe dados de um autor específico, devolve erro se não for encontrado ou id de busca nulo
         public async Task<IActionResult> Details(int? id)
         {
             if (id == null)
@@ -38,6 +41,7 @@ namespace TrabalhoAspNet.Controllers
         }
 
         // GET: Autores/Create
+        // Exibe a view para criar um novo autor
         public IActionResult Create()
         {
             return View();
@@ -46,6 +50,9 @@ namespace TrabalhoAspNet.Controllers
         // POST: Autores/Create
         // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        //Valida e cadastra um autor.
+        //Se os dados são válidos, adiciona e salva as mudanças
+        //Se não retorna a view com os dados do autor.
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create([Bind("Id,Nome,Nascimento,Nacionalidade")] Autor autor)
@@ -60,6 +67,7 @@ namespace TrabalhoAspNet.Controllers
         }
 
         // GET: Autores/Edit/5
+        //Exibe a view de edição para um autor específico
         public async Task<IActionResult> Edit(int? id)
         {
             if (id == null)
@@ -78,6 +86,7 @@ namespace TrabalhoAspNet.Controllers
         // POST: Autores/Edit/5
         // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        // Valida e edita um autor. Busca pelo id corresponde, atualiza os dados e salva as mudanças.
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Edit(int id, [Bind("Id,Nome,NascimentoDateTime,Nacionalidade")] Autor autor)
@@ -111,6 +120,7 @@ namespace TrabalhoAspNet.Controllers
         }
 
         // GET: Autores/Delete/5
+        //Exibe a view de confirmação de exclusão para um autor. Retorna erro se o id é nulo ou não encontra o autor.
         public async Task<IActionResult> Delete(int? id)
         {
             if (id == null)
@@ -129,11 +139,20 @@ namespace TrabalhoAspNet.Controllers
         }
 
         // POST: Autores/Delete/5
+        // verificar se existe em um livro
+        //Faz a exclusão de um autor. Busca pelo id e, se encontrar, remove do contexto e salva as mudanças.
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> DeleteConfirmed(int id)
         {
             var autor = await _context.Autores.FindAsync(id);
+            
+            var livro = await _context.Livros.FirstOrDefaultAsync(l => l.AutorId == id);
+            if (livro != null)
+            {
+                ModelState.AddModelError("AutorId", "Não é possivel excluir o autor, pois existe um livro associado.");
+            }
+            
             if (autor != null)
             {
                 _context.Autores.Remove(autor);
